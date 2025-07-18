aa9eb1b2debba54b1bdef4e53434a274fcbe2199
GauravGupta7 <gaurav.gupta2019@vitstudent.ac.in>
2025-07-18 22:29:41 +0530

    Model built and trained

--- notebooks/train.py
+++ notebooks/train.py
@@ -4,13 +4,32 @@ import numpy as np
 from DataPreprocessing import generating_training_sequences
 import logging
 import logging.config
+import tensorflow.keras as keras
+
+
+def buildModel(outputUnits, numberOfUnits, lossFunction, learningRate):
+    log.info("Building the model architecture...")
+    # Create the model arcitecture
+    inputLayer = keras.layers.Input(shape=(None, outputUnits))
+    x = keras.layers.LSTM(numberOfUnits[0])(inputLayer)
+    x = keras.layers.Dropout(0.2)(x)  # Adding dropout to prevent overfitting
+    outputLayer = keras.layers.Dense(outputUnits, activation='softmax')(x)  # Output layer with softmax activation
+    model = keras.Model(inputs=inputLayer, outputs=outputLayer)
+    
+    # Compiling the model
+    log.info("Compiling the model...")
+    model.compile(loss = lossFunction,
+                  optimizer=keras.optimizers.Adam(learning_rate=learningRate),
+                  metrics=['accuracy'])
+    model.summary()
+    
+    # Returning the model
+    log.info("Model architecture built successfully.")
+    return model
+    
+
 
 def main():
-    with open("config/application.yaml", "r") as file:
-        config = yaml.safe_load(file)
-        logging.config.dictConfig(config)
-    log = logging.getLogger(__name__)
-    
     # Loading configuration parameters
     sequence_length = config['sequence_length']
     save_path = config['save_path']
@@ -30,7 +49,39 @@ def main():
         targets = np.load(testTargetFolderPath+"/inputs.npy")            # Loading the targets from the saved file
         log.debug(f"Inputs shape: {inputs.shape}, Targets shape: {targets.shape}")
     log.info("Execution completed successfully...")
+    
+    # Build the model
+    outputUnits = 22  # Number of unique symbols in the dataset
+    lossFunction = config['lossFunction']
+    learningRate = config['learningRate']
+    numberOfUnits = config['numberOfUnits']
+    model = buildModel(outputUnits, numberOfUnits, lossFunction, learningRate)
+    
+    #Training the model
+    log.info("Starting model training...")
+    numberOfEpocs = config['numberOfEpocs']
+    trainingBatchSize = config['trainingBatchSize']
+    log.info("Inputs shape: %s", inputs.shape)
+    num_sequences = training_data_size//sequence_length
+    inputs = inputs[:num_sequences * sequence_length]
+    inputs = inputs.reshape((num_sequences, sequence_length, 22))
+    model.fit(inputs, 
+              targets, 
+              epochs=numberOfEpocs, 
+              batch_size=trainingBatchSize, 
+              validation_split=0.2)
+    log.info("Model training completed successfully!")
+    
+    # Saving the model
+    log.info("Saving the model...")
+    saveModelPath = config['saveModelPath'] + "/model.keras"
+    model.save(saveModelPath)
+    log.info(f"Model saved successfully at {saveModelPath}")
 
 
 if __name__ == "__main__":
+    with open("config/application.yaml", "r") as file:
+        config = yaml.safe_load(file)
+        logging.config.dictConfig(config)
+    log = logging.getLogger(__name__)
     main()
